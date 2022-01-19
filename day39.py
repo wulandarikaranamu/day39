69cd8b7af25b040709c98df8727e059621909ea6
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-19 19:23:48 +0700

    komentar

---
+++ day39.py
@@ -0,0 +1,23 @@
+print("list makanan merry")
+print("makanan sehari-hari merry adalah")
+
+print("list".center(60,"="))
+
+a = str(input("masukkan makanan pertama :"))
+b = str(input("masukkan makanan kedua :"))
+c = str(input("masukkan makanan ketiga :"))
+d = str(input("masukkan makanan keempat :"))
+
+makanan = [a, b, c, d]
+
+print("list".center(60,"="))
+
+print("kemudian kita akan menambahkan satu makanan favorit merry")
+e = str(input("masukkan makanan favorite merry :"))
+
+print("hasil".center(60,"="))
+
+print("4 sehat,5 sempurna")
+makanan.append(e)
+print("kesehatanmu = nilaimu")
+
